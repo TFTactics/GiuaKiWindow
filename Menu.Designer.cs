1b908846a450065c286148469e036791bd248062
Lê Đình Trường - 20110215 <anhma2720@gmail.com>
2022-03-29 20:28:30 +0700

    ver 0.0

---
+++ Menu.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace GameDiCanh
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCon = new System.Windows.Forms.Button();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnStart.Font = new System.Drawing.Font("Snap ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(206, 61);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(183, 54);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCon
+            // 
+            this.btnCon.BackColor = System.Drawing.Color.Transparent;
+            this.btnCon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCon.Font = new System.Drawing.Font("Snap ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCon.Location = new System.Drawing.Point(206, 137);
+            this.btnCon.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCon.Name = "btnCon";
+            this.btnCon.Size = new System.Drawing.Size(183, 50);
+            this.btnCon.TabIndex = 0;
+            this.btnCon.Text = "Tiếp tục";
+            this.btnCon.UseVisualStyleBackColor = false;
+            // 
+            // btnSetting
+            // 
+            this.btnSetting.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSetting.Font = new System.Drawing.Font("Snap ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetting.Location = new System.Drawing.Point(206, 210);
+            this.btnSetting.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Size = new System.Drawing.Size(183, 52);
+            this.btnSetting.TabIndex = 0;
+            this.btnSetting.UseVisualStyleBackColor = true;
+            this.btnSetting.Click += new System.EventHandler(this.btnSetting_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Snap ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(206, 288);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(183, 47);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::GameDiCanh.Properties.Resources._6241ba3d056db;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSetting);
+            this.Controls.Add(this.btnCon);
+            this.Controls.Add(this.btnStart);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Menu";
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCon;
+        private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Menu.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameDiCanh
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSetting_Click(object sender, EventArgs e)
+        {
+            btnCon.Hide();
+            btnExit.Hide();
+            btnStart.Hide();
+            btnSetting.Hide();
+            Setting();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            StageDemo stage0 = new StageDemo();
+            stage0.Size = new Size(1280, 720); 
+            stage0.ShowDialog();
+        }
+
+        public void Setting()
+        {
+            CheckBox NhacNen = new CheckBox();
+            NhacNen.Text = "Nhac Nen";
+            NhacNen.Location = new Point(206, 61);
+            NhacNen.Font = new Font("Snap ITC", 14);
+            NhacNen.AutoSize = true;
+            this.Controls.Add(NhacNen);
+
+            CheckBox NhacHieuUng = new CheckBox();
+            NhacHieuUng.Text = "SFX";
+            NhacHieuUng.Location = new Point(206, 137);
+            NhacHieuUng.Font = new Font("Snap ITC", 14);
+            NhacHieuUng.AutoSize = true;
+            this.Controls.Add(NhacHieuUng);
+
+            Button QuayLai = new Button();
+            QuayLai.Text = "RETURN";
+            QuayLai.Location = new Point(206, 268);
+            this.Controls.Add(QuayLai);
+
+            Button Reset = new Button();
+            Reset.Text = "RESET";
+            Reset.Location = new Point(206, 312);
+            this.Controls.Add(Reset);
+
+            QuayLai.Click += (s, e) =>
+            {
+                NhacHieuUng.Dispose();
+                NhacNen.Dispose();
+                QuayLai.Dispose();
+                btnCon.Show();
+                btnStart.Show();
+                btnExit.Show();
+                btnSetting.Show();
+            };
+        }
+    }
+}
